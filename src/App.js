6878d16131dbb33f82eaf19d48f3c4974b70d83b
scarlettiron <scottscarlett@gmail.com>
2023-10-22 23:03:25 -0400

    created rough draft of socket context

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
       <Routes>
         <AuthProvider>
           <Route exact path='/' element={<LoginSignup/>}></Route>
+          <Route path='/login' element={<LoginSignup/>}></Route>
+          <Route path='/signup' element={<LoginSignup/>}></Route>
           <InboxProvider>
             <PrivateRoute path='/dashboard' element={null}></PrivateRoute>
           </InboxProvider>
