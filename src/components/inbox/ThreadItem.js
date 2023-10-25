4d343a10219ebff572185f3c6abaec792879ba28
scarlettiron <scottscarlett@gmail.com>
2023-10-24 21:34:18 -0400

    added link to thread item component

--- src/components/inbox/ThreadItem.js
+++ src/components/inbox/ThreadItem.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {memo, useContext} from 'react'
+import { Link } from 'react-router-dom'
+import AuthContext from '../../context/AuthContext'
+import ProfileImageSmall from '../profile/ProfileImageSmall'
 
 const ThreadItem = (thread) => {
+    const {User} = useContext(AuthContext)
+    const receiver = User.id === thread.user1.id ? thread.user2 : thread.user1
   return (
+    <Link to={`/chat/${thread.id}`}>
     <div className='thread-item'>
+        <ProfileImageSmall
+        user={receiver}
+        />
 
+        <h3 className='text-primary'>{receiver.username}</h3>
     </div>
+    </Link>
   )
 }
 
-export default ThreadItem
+export default memo(ThreadItem)
